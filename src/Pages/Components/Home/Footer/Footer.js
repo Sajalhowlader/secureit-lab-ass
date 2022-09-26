c1d1c74aa4f4a525157d6f6000ee1433b164996f
@ ANTU <sajolhowlader08@gmail.com>
2022-09-26 17:27:03 +0600

    complete footer design

--- src/Pages/Components/Home/Footer/Footer.js
+++ src/Pages/Components/Home/Footer/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { FaFacebook, FaInstagram, FaTelegram, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaTelegram, FaTwitter } from "react-icons/fa";
+import footerLeft from "../../../../Assets/images/footer-dots-left.png";
+import footerRight from "../../../../Assets/images/footer-dots-right.png";
 const Footer = () => {
-  return <div className="footer_container">
-<FaTwitter className="footer_icon"/>
-<FaFacebook className="footer_icon"/>
-<FaInstagram className="footer_icon"/>
-<FaTelegram className="footer_icon"/>
-  </div>;
+  return (
+    <div className="footer_container">
+      <img src={footerLeft} alt="" />
+      <div><FaTwitter className="footer_icon" />
+      <FaFacebook className="footer_icon" />
+      <FaInstagram className="footer_icon" />
+      <FaTelegram className="footer_icon" /></div>
+      <img src={footerRight} alt="" />
+    </div>
+  );
 };
 
 export default Footer;
