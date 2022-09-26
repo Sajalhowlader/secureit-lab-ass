7c09497a349cb07a7c0bc6c53d92571f2f5b92e0
@ ANTU <sajolhowlader08@gmail.com>
2022-09-26 17:20:18 +0600

    add footer style

--- src/Pages/Components/Home/Footer/Footer.js
+++ src/Pages/Components/Home/Footer/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { FaFacebook, FaInstagram, FaTelegram, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
   return <div className="footer_container">
-    
+<FaTwitter className="footer_icon"/>
+<FaFacebook className="footer_icon"/>
+<FaInstagram className="footer_icon"/>
+<FaTelegram className="footer_icon"/>
   </div>;
 };
 
