0f68f76d1ec535e254a99d49e4d6931759e8e30b
@ ANTU <sajolhowlader08@gmail.com>
2022-09-26 18:28:50 +0600

    main ui second

--- src/Pages/Components/Home/MainUI/MainUi.js
+++ src/Pages/Components/Home/MainUI/MainUi.js
@@ -1,16 +1,23 @@
 import React from "react";
-
+import mainUiOne from "../../../../Assets/images/eq-image.png";
 const MainUi = () => {
   return (
     <section>
       <div className="infoContainer">
         <div className="infoOne">
           <p>DEPENDENCY TYPE </p>
-          <p className="infoSecond">CSA DETECTION</p>
+          <p className="infoTwo">CSA DETECTION</p>
         </div>
 
         <div className="infoSecond">
-            
+          <div>
+            <p>CATEGORE</p>
+            <p className="infoTwo">EQUIPMENT</p>
+          </div>
+          <span />
+          <div>
+            <img src={mainUiOne} alt="" />
+          </div>
         </div>
       </div>
     </section>
